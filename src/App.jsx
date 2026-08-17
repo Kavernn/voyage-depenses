9a771968d1aebe15c9b8f4fcc9a164e0505cf000
Kavernn <kavernn@gmail.com>
2026-08-17 15:34:01 -0400

    Revert "refine: simplify trip date section"
    This reverts commit 519910efc9cc30c3124fe922a24b86f1adceed07.

--- src/App.jsx
+++ src/App.jsx
@@ -1541,6 +1541,26 @@ function App() {
 
               </div>
 
+              {(data.trip.start || data.trip.end) && (
+                <div className="tripManagerDatePreview">
+                  <div>
+                    <span>DÉPART</span>
+                    <strong>
+                      {tripEditorFormatDate(data.trip.start)}
+                    </strong>
+                  </div>
+
+                  <i>→</i>
+
+                  <div>
+                    <span>RETOUR</span>
+                    <strong>
+                      {tripEditorFormatDate(data.trip.end)}
+                    </strong>
+                  </div>
+                </div>
+              )}
+
               {data.trip.start &&
                 data.trip.end &&
                 tripEditorDays && (
